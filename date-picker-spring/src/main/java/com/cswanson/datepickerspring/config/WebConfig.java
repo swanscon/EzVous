b30a470cf74959d663b5ac99866f66d7bf09ba7f
swanscon <swanscon@gmail.com>
2025-06-04 13:35:31 -0500

    fix: handle CORS Missing Allow Origin.

--- date-picker-spring/src/main/java/com/cswanson/datepickerspring/config/WebConfig.java
+++ date-picker-spring/src/main/java/com/cswanson/datepickerspring/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "https://ezvous.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
